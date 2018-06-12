4067f8694fe8e15c1697ccac7a70adc6ea9e4903
pythonflaskserverapps <pythonflaskserverapps@gmail.com>
2018-06-12 20:31:51 +0200

    Lichess api agent

--- bot.py
+++ bot.py
@@ -3,37 +3,82 @@ import json
 from serverutils.utils import read_string_from_file
 from serverutils.utils import getjsonbin, getjsonbinobj
 
+from lbot.lichess import Lichess
+
+#########################################################
+
+VERSION = "1.0.0"
+LICHESS_API_BASE_URL = "https://lichess.org/"
+
+#########################################################
+
 def getbinid():
     binid = read_string_from_file("binid.txt", "")
     if binid == "":
         return None
     return binid
 
+configobj = {}
+
+configname = None
+
 config = {}
 
+li = None
+
+def gettoken():
+    return config.get("token","xxxxxxxxxxxxxxxx")
+
+#########################################################
+
 def loadconfig():
-    global config
+    global configobj, configname, config
     binid = getbinid()
     if not ( binid is None ):            
         try:
             obj = getjsonbinobj(binid)
-            config = obj["config"]
-            print("config loaded ok")
+            configobj = obj["config"]
+            configname = configobj[0]
+            config = configobj[1]
+            print("config {} loaded ok".format(configname))            
         except:
-            print("loading config failed")
+            print("! loading config failed")
     else:
-        print("missing binid")
+        print("! missing binid")
 
 def printconfig():
     print("printing config")
-    formatted = json.dumps(config)
+    formatted = json.dumps(configobj)
     print(formatted)
     print("config printed")
+
+def loadprofile():    
+    try:
+        print("loading profile")
+        profile = li.get_profile()
+        print("profile loaded")
+        print(profile)
+    except:
+        print("! loading profile failed")
+
+def createli():
+    global li
+    token = gettoken()
+    print("creating lichess api agent", token, LICHESS_API_BASE_URL, VERSION)
+    li = Lichess(token, LICHESS_API_BASE_URL, VERSION)
+    print("lichess api agent created")
+    print("testing lichess api agent")
+    loadprofile()
+
+#########################################################
 
 while True:
     cmd = input("").rstrip()
     if cmd == "lc":
         loadconfig()
         printconfig()
+        createli()
+    elif cmd == "lp":
+        loadprofile()
     else:
         print("echo", cmd)
