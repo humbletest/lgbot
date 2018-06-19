285872f2c176620fa0ab6c9c0ee858bbeaba1b3d
humbletest <humbletest@gmail.com>
2018-06-19 12:37:40 +0200

    PvInfos class bin

--- static/js/app.js
+++ static/js/app.js
@@ -1,5 +1,5 @@
 "use strict";
-// Transcrypt'ed from Python, 2018-06-18 17:02:00
+// Transcrypt'ed from Python, 2018-06-19 12:24:35
 function app () {
     var __symbols__ = ['__py3.6__', '__esv5__'];
     var __all__ = {};
@@ -2682,7 +2682,7 @@ function app () {
 				__super__ (Log, '__init__') (self, 'div');
 				self.width = args.py_get ('width', 600);
 				self.height = args.py_get ('height', 400);
-				self.maxitems = args.py_get ('maxitems', 20);
+				self.maxitems = args.py_get ('maxitems', 100);
 				self.ac ('logdiv');
 				self.py_items = list ([]);
 				self.resize (self.width, self.height);
