347b5bc032892e7962cf108a1412b736b34a3eea
humbletest <humbletest@gmail.com>
2018-06-27 08:46:48 +0200

    Latest Stockfish ( Linux )

---
+++ ra.bat
@@ -0,0 +1,8 @@
+heroku ps:scale web=%1 -a lgbotapi
+heroku ps:scale web=%1 -a lgbotatom
+heroku ps:scale web=%1 -a lgbotrand
+heroku ps:scale web=%1 -a lgbotcapt
+heroku ps:scale web=%2 -a lgbotapi
+heroku ps:scale web=%2 -a lgbotatom
+heroku ps:scale web=%2 -a lgbotrand
+heroku ps:scale web=%2 -a lgbotcapt
